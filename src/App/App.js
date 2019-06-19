007d09b949567ac27e0a995b9bfe3001f885b525
Greg Stephen <GregAStephen@gmail.com>
2019-06-19 07:29:55 -0500

    Set it up

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.scss';
+
+class App extends React.Component {
+  // state = {
+  //   dogs: [],
+  // }
+
+  // componentDidMount() {
+  //   this.setState({ dogs: myDogs });
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <button className="btn btn-danger">HELP</button>
+      </div>
+    );
+  }
+}
+
+export default App;
