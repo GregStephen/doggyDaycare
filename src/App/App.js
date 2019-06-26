0017a203a5f654c7528e4f9d39d00f159598edf8
Greg Stephen <GregAStephen@gmail.com>
2019-06-26 08:13:21 -0500

    got the navbar set up

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import Navbar from '../components/Navbar/Navbar';
 import Home from '../components/Home/Home';
 import Auth from '../components/Auth/Auth';
 import connection from '../helpers/data/connection';
@@ -39,6 +40,7 @@ class App extends React.Component {
     };
     return (
       <div className="App">
+        <Navbar authed={authed} />
        {loadComponent()}
       </div>
     );
