3feff7c4088864d2765c0b3e294ce527d6635b67
Greg Stephen <GregAStephen@gmail.com>
2019-06-26 08:07:45 -0500

    added auth verification

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import Home from '../components/Home/Home';
+import Auth from '../components/Auth/Auth';
+import connection from '../helpers/data/connection';
+
 import './App.scss';
 
+connection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
     return (
       <div className="App">
-       <Home />
+       {loadComponent()}
       </div>
     );
   }
